8752b46923fc45d784d8d2e8a9f0715d26c616a8
Volker Schaefer <schaefer.volker@googlemail.com>
2020-06-24 19:50:24 +0200

    Added eslint and PoC for getting data from Elasticsearch

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
-'use strict';
+'use strict'
 
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'http://cranach-elk:9200/' });
 const express = require('express');
+
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
+
+app.use(cors());
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  client.search({
+    index: 'paintings_de',
+    body: {
+      query: {
+        match_all: {}
+      },
+    },
+  }, (err, result) => {
+    if (err) console.log(err);
+    res.send(result);
+  });
 });
 
 app.listen(PORT);
