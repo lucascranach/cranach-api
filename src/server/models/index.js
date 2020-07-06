173a4c50e25309449a77c7338286828e920fdc29
Volker Schaefer <schaefer.volker@googlemail.com>
2020-07-06 14:53:34 +0200

    Added filter aggregations

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,28 +1,71 @@
 /* eslint-disable no-underscore-dangle */
 const { esclient, index, type } = require('../../elastic');
+const { mappings } = require('../mappings');
 
 async function submitESSearch(params) {
+  const currentAggs = { };
+  if (params.filter) {
+    params.filter.forEach((filterItem) => {
+      currentAggs[filterItem.key] = {
+        terms: {
+          field: filterItem.display_value,
+        },
+        aggs: {
+          [filterItem.key]: {
+            terms: {
+              field: filterItem.value,
+            },
+          },
+        },
+      };
+    });
+  }
+
   const esParams = {
     from: params.req.from || 0,
     size: params.req.size || 100,
     index: params.index,
     type: params.type,
-    body: params.query,
+    // body: { params.query, aggs: currentAggs},
+    body: {
+      query: params.query,
+      aggs: currentAggs,
+    },
   };
-
-  const result = await esclient.search(esParams);
-  return result;
+  try {
+    const result = await esclient.search(esParams);
+    return result;
+  }
+  catch(error) {
+    console.error(error.meta.body.error);
+    throw new Error('Elasticsearch does not provide a respons');
+  }
 }
 function aggregateESResult(params) {
-  const { body: { took, hits } } = params;
-
-  console.log('took', took);
-
-  const result = { };
+  const { body: { took, hits, aggregations = { } } } = params;
+  const filter = {};
   const meta = { };
+  const result = { };
+
   meta.took = took;
   meta.hits = hits.total.value;
 
+  // TODO: Create recursive function
+  // aggregate Filter
+  const aggregationKeys = Object.keys(aggregations);
+  aggregationKeys.forEach((aggregationKey) => {
+    const { buckets } = aggregations[aggregationKey];
+    const currentFilter = buckets.map((bucket) => {
+      const ret = {};
+      ret.doc_count = bucket.doc_count;
+      ret.value = bucket.key;
+      ret.display_value = buckets[0].key;
+      return ret;
+    });
+    filter[aggregationKey] = currentFilter;
+  });
+
+  // aggregate results
   const results = hits.hits.map((hit) => ({
     _data_all: hit._source,
     id: hit._id,
@@ -33,6 +76,10 @@ function aggregateESResult(params) {
     score: hit._score,
   }));
 
+  if (Object.keys(filter).length > 0) {
+    result.filters = filter;
+  }
+
   result.meta = meta;
   result.results = results;
   return result;
@@ -40,10 +87,8 @@ function aggregateESResult(params) {
 
 async function getSingleGraphic(params) {
   const query = {
-    query: {
-      match: {
-        _id: params.id,
-      },
+    match: {
+      _id: params.id,
     },
   };
 
@@ -63,23 +108,25 @@ async function getSingleGraphic(params) {
 
 async function getGraphics(req) {
   const query = {
-    query: {
-      match_all: { },
-    },
+    match_all: { },
   };
+
+  const filterConfig = mappings.filter((mapping) => mapping.filter === true);
 
   const result = await submitESSearch({
     req,
     index,
     type,
     query,
+    filter: filterConfig,
   });
-  const { meta, results } = aggregateESResult(result);
+  const { meta, results, filters } = aggregateESResult(result);
 
-  return {
-    meta,
-    results,
-  };
+  const ret = {};
+  ret.meta = meta;
+  ret.results = results;
+  ret.filters = filters;
+  return ret;
 }
 
 module.exports = {
