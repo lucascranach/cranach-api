8e09f7780a17019543d242d50b49a1cba28bf2c9
Volker Schaefer <schaefer.volker@googlemail.com>
2020-07-06 14:54:01 +0200

    Added mapping config

---
+++ src/server/mappings/index.js
@@ -0,0 +1,32 @@
+const mappings = [{
+  display_value: 'dating.begin',
+  key: 'dating_begin',
+  filter: true,
+  value: 'dating.begin',
+},
+{
+  display_value: 'dating.end',
+  key: 'dating_end',
+  filter: true,
+  value: 'dating.end',
+},
+{
+  display_value: 'classification.classification.keyword',
+  key: 'classification',
+  filter: true,
+  value: 'classification.classification.keyword',
+},
+{
+  display_value: 'images.infos.maxDimensions.width',
+  key: 'size_width',
+  filter: true,
+  value: 'images.infos.maxDimensions.width',
+},
+{
+  display_value: 'images.infos.maxDimensions.height',
+  key: 'size_height',
+  filter: true,
+  value: 'images.infos.maxDimensions.height',
+}];
+
+module.exports = { mappings };
