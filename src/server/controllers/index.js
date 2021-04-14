2a40bca9d40c5e15f39c57aec8bdfc0f0a5d9307
Volker Schaefer <schaefer.volker@googlemail.com>
2021-04-14 11:30:40 +0200

    Adds basic authentication

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -14,8 +14,7 @@ async function getSingleItem(req, res) {
     const result = await model.getSingleItem(params);
     res.json({ data: result });
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false, error: 'Unknown error.' });
+    res.status(500).json({ success: false, error: err.message });
   }
 }
 
@@ -25,8 +24,7 @@ async function getItems(req, res) {
     const result = await model.getItems(query);
     res.json({ data: result });
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false, error: 'Unknown error.' });
+    res.status(500).json({ success: false, error: err.message });
   }
 }
 
