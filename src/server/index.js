3a18559425d0806ab5b1e8e0a035cbdd47bf8c81
Volker Schaefer <schaefer.volker@googlemail.com>
2021-04-30 17:29:40 +0200

    Formats code

--- src/server/index.js
+++ src/server/index.js
@@ -11,13 +11,39 @@ function start() {
   if (!process.env.ELASTICSEARCH_USERNAME || !process.env.ELASTICSEARCH_PASSWORD) {
     throw new Error('Environt variables are missing');
   }
+<<<<<<< Updated upstream
+=======
+  let httpServer = null;
+
+  if (useHTTPS === true) {
+    if (!process.env.DOMAIN) {
+      throw new Error('Enviroment variable DOMAIN is missing');
+    }
+
+    const domain = process.env.DOMAIN;
+    const options = {
+      cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`, 'utf8'),
+      key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`, 'utf8'),
+    };
+
+    httpServer = https.createServer(options, app);
+  } else {
+    httpServer = http.createServer(app);
+  }
+
+  httpServer.listen(port, () => console.log(`Server ready on port ${port}`));
+>>>>>>> Stashed changes
 
   return app.use(cors())
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
     .use('/', routes)
+<<<<<<< Updated upstream
     .use((_req, res) => res.status(404).json({ success: false, error: 'Route not found' }))
     .listen(port, () => console.log(`Server ready on port ${port}`));
+=======
+    .use((_req, res) => res.status(404).json({ success: false, error: 'Route not found' }));
+>>>>>>> Stashed changes
 }
 
 module.exports = {
