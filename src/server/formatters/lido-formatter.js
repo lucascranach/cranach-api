9450503e1b5842fc10acdb539f124f3c4874e570
Volker Schaefer <v.schaefer@klickmeister.de>
2026-02-19 10:46:26 +0100

    fix: update LIDO schema location to version 1.1 in XML formatter

--- src/server/formatters/lido-formatter.js
+++ src/server/formatters/lido-formatter.js
@@ -59,7 +59,7 @@ class LidoFormatter extends BaseFormatter {
       .ele('lido:lidoWrap', {
         'xmlns:lido': 'http://www.lido-schema.org',
         'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-        'xsi:schemaLocation': 'http://www.lido-schema.org http://www.lido-schema.org/schema/v1.0/lido-v1.0.xsd',
+        'xsi:schemaLocation': 'http://www.lido-schema.org http://www.lido-schema.org/schema/v1.1/lido-v1.1.xsd',
       });
 
     const lido = root.ele('lido:lido');
@@ -184,7 +184,7 @@ class LidoFormatter extends BaseFormatter {
       .ele('lido:lidoWrap', {
         'xmlns:lido': 'http://www.lido-schema.org',
         'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-        'xsi:schemaLocation': 'http://www.lido-schema.org http://www.lido-schema.org/schema/v1.0/lido-v1.0.xsd',
+        'xsi:schemaLocation': 'http://www.lido-schema.org http://www.lido-schema.org/schema/v1.1/lido-v1.1.xsd',
       });
 
     const lido = root.ele('lido:lido');
