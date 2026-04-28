351968cf88ac6263d633a0ba1fdda5c5be8fca80
Volker Schaefer <volker.schaefer@th-koeln.de>
2026-04-28 11:07:48 +0200

    feat(lido): update resourceSource to use dynamic repository ID and name

--- src/server/formatters/lido-formatter.js
+++ src/server/formatters/lido-formatter.js
@@ -1329,17 +1329,18 @@ class LidoFormatter extends BaseFormatter {
       const imageMetadata = imageMetadataMap[image.id] || {};
       const sourceName = (imageMetadata.source?.de || '').replace(/^©\s*/, '');
       const createdName = (imageMetadata.created?.de || '').replace(/^©\s*/, '') || 'Cranach Digital Archive';
+      const sourceRepoData = getRepositoryID(sourceName);      
       const createdRepoData = getRepositoryID(createdName);
 
       resourceSet.ele('lido:resourceSource')
         .ele('lido:legalBodyID', {
           'lido:type': 'http://terminology.lido-schema.org/lido00099',
         })
-        .txt('https://d-nb.info/gnd/1073160734')
+        .txt(sourceRepoData.repositoryID)
         .up()
         .ele('lido:legalBodyName')
         .ele('lido:appellationValue')
-        .txt('Technische Hochschule Köln');
+        .txt(sourceName);
 
       //   │  └─ lido:rightsResource
       const rightsResource = resourceSet.ele('lido:rightsResource');
