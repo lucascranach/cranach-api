88b44428f7eaf592352ee3201d4be5ee09e27469
Volker Schaefer <volker.schaefer@th-koeln.de>
2026-05-19 10:53:56 +0200

    refactor(lido): reorder objectDescriptionSet blocks and restore Anmerkung fallback
    Move the additional text information set directly after the main
    description so all untyped sets are grouped first. Re-add the fallback
    branch that emits a single objectDescriptionSet[type="Anmerkung"] when
    footnotes are present but carry no [n] markers, since source data cannot
    be normalised. The resulting element order is: description (no type),
    additional text (no type), Anmerkung/Forschungsgeschichte, Quellen,
    Provenienz.

--- src/server/formatters/lido-formatter.js
+++ src/server/formatters/lido-formatter.js
@@ -512,6 +512,26 @@ class LidoFormatter extends BaseFormatter {
         .txt(descriptiveNoteValueDe.citation);
     }
 
+    //   │  ├─ lido:objectDescriptionSet (Additional text information)
+    if (languageData.de.additional_text_information_text
+      && languageData.de.additional_text_information_text[0]
+      && languageData.de.additional_text_information_text[0].text) {
+      const additionalTextInfoDe = this.extractTextAndCitation(
+        languageData.de.additional_text_information_text[0].text,
+      );
+
+      const additionalTextDescriptionSet = objectDescriptionWrap.ele('lido:objectDescriptionSet');
+
+      additionalTextDescriptionSet.ele('lido:descriptiveNoteValue', {
+        'xml:lang': 'de',
+      }).txt(additionalTextInfoDe.text);
+
+      if (additionalTextInfoDe.citation !== '') {
+        additionalTextDescriptionSet.ele('lido:sourceDescriptiveNote')
+          .txt(additionalTextInfoDe.citation);
+      }
+    }
+
     //   │  ├─ lido:objectDescriptionSet (Footnotes / Anmerkungen)
     if (splitDe.footnotes || splitEn.footnotes) {
       const deAnnotations = this.parseAnnotations(splitDe.footnotes);
@@ -521,17 +541,30 @@ class LidoFormatter extends BaseFormatter {
       const enMap = new Map(enAnnotations.map(a => [a.number, a.text]));
       const allNumbers = [...new Set([...deMap.keys(), ...enMap.keys()])].sort((a, b) => a - b);
 
-      for (const num of allNumbers) {
-        const annotationSet = objectDescriptionWrap.ele('lido:objectDescriptionSet', {
-          'lido:type': 'Forschungsgeschichte',
-          'lido:sortedSetOrder': num,
+      if (allNumbers.length > 0) {
+        for (const num of allNumbers) {
+          const annotationSet = objectDescriptionWrap.ele('lido:objectDescriptionSet', {
+            'lido:type': 'Forschungsgeschichte',
+            'lido:sortedSetOrder': num,
+          });
+
+          if (deMap.has(num)) {
+            annotationSet.ele('lido:descriptiveNoteValue', { 'xml:lang': 'de' }).txt(deMap.get(num));
+          }
+          if (enMap.has(num)) {
+            annotationSet.ele('lido:descriptiveNoteValue', { 'xml:lang': 'en' }).txt(enMap.get(num));
+          }
+        }
+      } else {
+        const footnotesDescriptionSet = objectDescriptionWrap.ele('lido:objectDescriptionSet', {
+          'lido:type': 'Anmerkung',
         });
 
-        if (deMap.has(num)) {
-          annotationSet.ele('lido:descriptiveNoteValue', { 'xml:lang': 'de' }).txt(deMap.get(num));
+        if (splitDe.footnotes) {
+          footnotesDescriptionSet.ele('lido:descriptiveNoteValue', { 'xml:lang': 'de' }).txt(splitDe.footnotes);
         }
-        if (enMap.has(num)) {
-          annotationSet.ele('lido:descriptiveNoteValue', { 'xml:lang': 'en' }).txt(enMap.get(num));
+        if (splitEn.footnotes) {
+          footnotesDescriptionSet.ele('lido:descriptiveNoteValue', { 'xml:lang': 'en' }).txt(splitEn.footnotes);
         }
       }
     }
@@ -583,26 +616,6 @@ class LidoFormatter extends BaseFormatter {
     }
 
 
-    //   │  ├─ lido:objectDescriptionSet (Additional text information)
-    if (languageData.de.additional_text_information_text
-      && languageData.de.additional_text_information_text[0]
-      && languageData.de.additional_text_information_text[0].text) {
-      const additionalTextInfoDe = this.extractTextAndCitation(
-        languageData.de.additional_text_information_text[0].text,
-      );
-
-      const additionalTextDescriptionSet = objectDescriptionWrap.ele('lido:objectDescriptionSet');
-
-      additionalTextDescriptionSet.ele('lido:descriptiveNoteValue', {
-        'xml:lang': 'de',
-      }).txt(additionalTextInfoDe.text);
-
-      if (additionalTextInfoDe.citation !== '') {
-        additionalTextDescriptionSet.ele('lido:sourceDescriptiveNote')
-          .txt(additionalTextInfoDe.citation);
-      }
-    }
-    //   │  └─ End: lido:objectDescriptionSet (Additional text information)
     //   └─ End: lido:objectDescriptionWrap
 
     //   ├─ lido:objectMeasurementsWrap
